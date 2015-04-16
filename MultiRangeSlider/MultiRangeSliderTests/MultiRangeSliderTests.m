2ca3192a66e737b4a0b794d6d07925951c5b165a
DavidLam <david.lam@zensis.com>
2015-04-16 17:21:46 +0800

    initial commit

---
+++ MultiRangeSlider/MultiRangeSliderTests/MultiRangeSliderTests.m
@@ -0,0 +1,40 @@
+//
+//  MultiRangeSliderTests.m
+//  MultiRangeSliderTests
+//
+//  Created by Zensis on 16/4/15.
+//  Copyright (c) 2015 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MultiRangeSliderTests : XCTestCase
+
+@end
+
+@implementation MultiRangeSliderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
